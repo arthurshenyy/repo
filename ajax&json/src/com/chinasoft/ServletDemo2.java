d3bee5a4f2519fe39fc2435f8db3fccc1fe9e8cb
jack <462503082@qq.com>
2020-08-13 14:18:38 +0800

    tijiao

--- ajax&json/src/com/chinasoft/ServletDemo2.java
+++ ajax&json/src/com/chinasoft/ServletDemo2.java
@@ -13,20 +13,7 @@ import java.io.IOException;
 @WebServlet("/demo2")
 public class ServletDemo2 extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding("utf-8");
-        String user = req.getParameter("user");
-        System.out.println(user);
-        User u = JSON.parseObject(user, User.class);
-        System.out.println(u);
-//        设置响应 让客户端以json的形式打开数据
-        resp.setContentType("application/json;charset=utf-8");
-        resp.getWriter().write(JSON.toJSONString(u));
-
-
-//        List<User>
-//        Map<String,String[]>
-
-
+      
 
 
 
@@ -34,7 +21,4 @@ public class ServletDemo2 extends HttpServlet {
 
     }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doPost(req, resp);
-    }
 }
